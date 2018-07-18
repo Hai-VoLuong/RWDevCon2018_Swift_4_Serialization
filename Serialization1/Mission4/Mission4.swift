d291d51b864ab4f07e111e863f9d370202c84f66
MAC <>
2018-07-18 22:07:40 +0700

    update code

--- Serialization1/Mission4/Mission4.swift
+++ Serialization1/Mission4/Mission4.swift
@@ -20,36 +20,35 @@ import XCTest
 // Mars Rover Implementation
 
 struct Rover: Codable, Equatable {
-  
-  enum CodingKeys: String, CodingKey {
-    case name = "rover_name"
-    case missionData = "mission_data"
-  }
-  
-  var name: String
-  
-  struct MissionData: Codable, Equatable {
-    var photos: [Photo]?
-  }
-  var missionData: MissionData
+    
+    var name: String
+    var missionData: MissionData
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "rover_name"
+        case missionData = "mission_data"
+    }
+    
+    struct MissionData: Codable, Equatable {
+        var photos: [Photo]?
+    }
+    
 }
 
 // Testing
-
 class Mission4: XCTestCase {
-
-  let curiosity = Rover(name: "Curiosity", missionData: Rover.MissionData(photos:
-    [Photo(url: URL(string:"https://go.nasa.gov/2nquyg9"),
-           camera: .navcams,
-           time: Sols(1949))]))
-  
-  let curiosityJSON = """
+    
+    let curiosity = Rover(name: "Curiosity", missionData: Rover.MissionData(photos:
+        [Photo(url: URL(string:"https://go.nasa.gov/2nquyg9"),
+               camera: .navcams,
+               time: Sols(1949))]))
+    
+    let curiosityJSON = """
 {"rover_name":"Curiosity","mission_data": {"photos":[{"url":"https://go.nasa.gov/2nquyg9","camera":"navcams","time":{"sols":1949}}]}}
 """
-  
-  func testCodable() throws {
-    XCTFail()
-    // try roundTripTest(item: curiosity)
-    // try archiveTest(json: curiosityJSON, expected: curiosity)
-  }
+    
+    func testCodable() throws {
+         try roundTripTest(item: curiosity)
+         try archiveTest(json: curiosityJSON, expected: curiosity)
+    }
 }
